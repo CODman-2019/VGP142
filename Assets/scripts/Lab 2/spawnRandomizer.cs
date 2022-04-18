6f5da66f4ad70ac4da07e96690aefa55e88c8e7c
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2022-04-18 14:25:23 -0400

    Lab 2 complete (full)

--- Assets/scripts/Lab 2/spawnRandomizer.cs
+++ Assets/scripts/Lab 2/spawnRandomizer.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class spawnRandomizer : MonoBehaviour
 {
+    public int spawnNum;
+    public GameObject spawner;
     GameObject[] spawns;
     GameObject[] selectedSpawns;
-    GameObject[] placedSpawns;
 
     // Start is called before the first frame update
     void Start()
     {
-        //spawns = FindObjectsOfType<>;   
+        spawns = GameObject.FindGameObjectsWithTag("Spawner");
+        selectedSpawns = new GameObject[spawnNum];
+
+        for(int x = 0; x < spawnNum; x++)
+        {
+            int position = Random.Range(0, spawns.Length);
+            
+            for(int y = 0; y < spawnNum; y++)
+            {
+                if(selectedSpawns[y] == null)
+                {
+                    selectedSpawns[y] = spawns[position];
+                    break;
+                }
+                else if(selectedSpawns[y] == spawns[position])
+                {
+                    x--;
+                    break;
+                }
+            }
+        }
+
+        for (int z = 0; z < spawnNum; z++)
+        {
+            Instantiate(spawner, selectedSpawns[z].transform);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
